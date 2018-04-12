720db999ec33c8caeaa11df7f8d27f15d68ae8db
mid2sick <34702573+mid2sick@users.noreply.github.com>
2018-04-12 22:09:38 +0800

    Update B_cooperate.cpp

--- B_coorperate/src/B_cooperate.cpp
+++ B_coorperate/src/B_cooperate.cpp
@@ -28,8 +28,6 @@ class GyroSample : public frc::IterativeRobot {
 
 	WPI_TalonSRX *leftDrive_3 = new WPI_TalonSRX(1);
 	WPI_TalonSRX *rightDrive_1 = new WPI_TalonSRX(2);
-//	WPI_TalonSRX *leftDrive_4 = new WPI_TalonSRX(4);
-//	WPI_TalonSRX *rightDrive_2 = new WPI_TalonSRX(2);
 	VictorSP armBack {4};
 	VictorSP armLeft {5};
 	VictorSP armRight {0};
@@ -37,94 +35,40 @@ class GyroSample : public frc::IterativeRobot {
 	VictorSP dumpright {2};
 	VictorSP intakeRight {1};
 	VictorSP intakeLeft {6};
-	string gameData;
 	Timer t;
-//	AnalogGyro gyro;
-	double angle = 0, kp = 0.03;
-	int flag = 0;
+
 	class RobotDrive myRobot { leftDrive_3, rightDrive_1};
 	class Joystick *m_stick = new Joystick(0);
 	class Joystick *m_stick2 = new Joystick(1);
-	AnalogInput *encoderArmBack = new AnalogInput(0);
-	PIDController ParmBack{0.02, 0, 0, encoderArmBack, &armBack};
-	long value = encoderArmBack->GetAccumulatorValue();
-	long count = encoderArmBack->GetAccumulatorCount();
-//	encoderArmBack->SetAccumul atorInitialValue(0);
 
-	//int value = encoderArmBack->GetAccumulatorValue();
+
 
 public:
 
 
-/*	GyroSample():
-	  gyro(1)
-	  {
 
-	  }
-
-*/
 
 
 	void RobotInit()
 	{
 		CameraServer::GetInstance()->StartAutomaticCapture(0);
 		CameraServer::GetInstance()->StartAutomaticCapture(1);
-	//	CameraServer::GetInstance()->StartAutomaticCapture(2);
+		CameraServer::GetInstance()->StartAutomaticCapture(2);
 	}
 
 	void AutonomousInit(){
 		t.Reset();
 		t.Start();
-//		gyro.Reset();
-//		gameData = frc::DriverStation::GetInstance().GetGameSpecificMessage();
-
-	/*	if( angle != 0 )
-		{
-			myRobot.ArcadeDrive( 0, 0.3 );
-		}*/
 	}
 
 	void AutonomousPeriodic(){
 
-//		if( t.Get() < 1.0 )
-//		{
-//			gyro.Calibrate();
-//		}
-
-/*		if(gameData.length() > 0)
-		{
-			if(gameData[0] == 'L')
-			{
-							//Put left auto code here
-			}
-			else
-			{
-							//Put right auto code here
-			}
-		}
 
 
-		if( t.Get() < 5.0  && t.Get() > 1.0 )
-		{
-			angle=gyro.GetAngle();
-			double turningValue = (0-angle) * 2.5;
-			myRobot.ArcadeDrive( 0.6 , -0.25);
-			cout<<"angle:"<<angle<<endl;
-			cout<<"turningValue:"<<turningValue<<endl;
-		}
-		else if( t.Get() >= 8.0 )
-		{
-			myRobot.ArcadeDrive( 0.0 ,0.0 );
-			t.Stop();
-		}
-*/
+
 		if( t.Get() < 3.0   )
 		{
-		//	angle=gyro.GetAngle();
-		//	double turningValue = (0-angle) * 2.5;
 			myRobot.ArcadeDrive( 0.6 , 0);
-		//	cout<<"angle:"<<angle<<endl;
-		//	cout<<"turningValue:"<<turningValue<<endl;
 		}
 		else if( t.Get() >= 3.0)
 		{
@@ -135,17 +79,11 @@ public:
 	}
 
 	void TeleopInit(){
-//		gyro->Reset();
-//		gyro->Calibrate();
-		encoderArmBack->ResetAccumulator();
+
 	}
 
 	void TeleopPeriodic(){
 
-		// AccumulatorResult *result = new AccumulatorResult();
-		value = encoderArmBack->GetAccumulatorValue();
-		count = encoderArmBack->GetAccumulatorCount();
-		cout<<"value:"<<value<<" count:"<<count<<endl;
 
 		if(m_stick->GetRawButton(1)){
 			intakeLeft.Set(-1);
@@ -174,30 +112,10 @@ public:
 			dumpright.Set(0);
 		}
 
-/*		if( m_stick2->GetRawAxis(3) )
-		{
-			armBack.Set(0.8);
-		}
-		else if( m_stick2->GetRawButton(6) )
-		{
-			armBack.Set(-0.8);
-		}
-		else
-		{
-			armBack.Set(0);
-		}
-*/
 
 		armBack.Set( m_stick2->GetRawAxis(1) * 1.0 );
 
-		///cout<<"flag="<<flag<<endl;
-/*   	if( m_stick->GetRawButton(8) )
-		{
-			flag=!flag;
-		}
-*/
-//		if( flag )
-//		{
+
 
 		if( m_stick2->GetRawAxis(2) )
 		{
@@ -224,12 +142,10 @@ public:
 		{
 			armRight.Set( m_stick2->GetRawAxis(4) * 0.7 );
 		}
-//		}
 
 
 
-//		angle=gyro->GetAngle();
-//		turningValue = (0-angle)*0.005;
+
 
 		if( m_stick->GetRawAxis(2) )
 		{
@@ -240,15 +156,7 @@ public:
 			myRobot.ArcadeDrive( m_stick->GetRawAxis(1) * -0.6, m_stick->GetRawAxis(0) * -0.6 );
 		}
 
-	//	cout<< m_stick->GetRawAxis(0) * (-0.6) <<endl;
 
-/*		if( !flag )
-		{
-			armRight.Set( m_stick->GetRawAxis(4) * 1.0 );
-            armLeft.Set( m_stick->GetRawAxis(4) * 1.0 );
-		}
-*/
-   //     cout<< m_stick->GetRawAxis(0) *0.6 <<endl;
 
 		frc::Wait(0.005);
 
